ad26fbc4a96ad9966b89f469a8a497586173eb3b
Muhammad Talal Anwar <talalanwar@outlook.com>
2019-12-29 14:00:03 +0100

    Add collector for Swift dispersion report

--- main.go
+++ main.go
@@ -17,17 +17,25 @@ package main
 import (
 	"context"
 	"net/http"
+	"os"
+	"os/exec"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sapcc/go-bits/httpee"
 	"github.com/sapcc/go-bits/logg"
+	"github.com/sapcc/swift-health-exporter/collectors"
 )
 
 func main() {
-	// this port has been allocated for a Swift health exporter
+	swiftDispersionReportPath := getExecutablePath("SWIFT_DISPERSION_REPORT", "swift-dispersion-report")
+
+	prometheus.MustRegister(collectors.NewDispersionCollector(swiftDispersionReportPath))
+
+	// this port has been allocated for Swift health exporter
 	// See: https://github.com/prometheus/prometheus/wiki/Default-port-allocations
 	listenAddr := ":9520"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { w.Write(landingPageBytes) })
+	http.HandleFunc("/", landingPageHandler)
 	http.Handle("/metrics", promhttp.Handler())
 	logg.Info("listening on " + listenAddr)
 	err := httpee.ListenAndServeContext(httpee.ContextWithSIGINT(context.Background()), listenAddr, nil)
@@ -36,7 +44,25 @@ func main() {
 	}
 }
 
-var landingPageBytes = []byte(`<html>
+// getExecutablePath gets the path to an executable from the environment
+// variable using the envKey (if defined).
+// Otherwise it attempts to find the path in the directories defined in the
+// "PATH" environment variable.
+func getExecutablePath(envKey, fileName string) string {
+	val := os.Getenv(envKey)
+	if val != "" {
+		return val
+	}
+
+	path, err := exec.LookPath(fileName)
+	if err != nil {
+		logg.Fatal("could not find an executable named %q in the directories named by the PATH environment variable", fileName)
+	}
+	return path
+}
+
+func landingPageHandler(w http.ResponseWriter, r *http.Request) {
+	pageBytes := []byte(`<html>
 <head><title>Swift Health Exporter</title></head>
 <body>
 <h1>Swift Health Exporter</h1>
@@ -44,3 +70,9 @@ var landingPageBytes = []byte(`<html>
 <p><a href="https://github.com/sapcc/swift-health-exporter">Source Code</a></p>
 </body>
 </html>`)
+
+	_, err := w.Write(pageBytes)
+	if err != nil {
+		logg.Error(err.Error())
+	}
+}
